9a90a6fa388f00f0699498d23024c7c37330f0a0
unknown <akhilice13@gmail.com>
2021-07-30 11:49:50 +0530

    fixed useEffect infinite loop

--- frontend/src/components/BlogList.js
+++ frontend/src/components/BlogList.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Card, Button, Container } from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
-import {blogListAction} from '../reactRedux/actions/blogAction'
-
-
+import { blogListAction } from '../reactRedux/actions/blogAction'
 
 const BlogList = () => {
 
@@ -12,50 +10,65 @@ const BlogList = () => {
 
   const blogList = useSelector((state) => state.blogList)
 
-  const {loading, blogs, error} = blogList
+  const { loading, blogs, error } = blogList
   console.log(blogs)
-  
-  
 
+ 
   useEffect(() => {
     dispatch(blogListAction())
-  }, [dispatch,blogs])
-    return (
-        <div>
-          {loading ? (
+  }, [dispatch])
+  return (
+    <div>
+      {loading ? (
         <p style={{ marginTop: '400px', color: 'black' }}>Loading Blogs</p>
       ) : (
-             <Row xs={1} sm={2} md={3} style={{ margin: '20px' }} className="g-4">
-        {blogs.map((cards) => (
-          <Col  key={cards.createdAt} >
-            <Card className="customCard">
-              
-              <Card.Img
-              style={{height:"40vh", width:"100%"}}
-                variant="top"
-                src={`${cards.file}`}
-                bsPrefix="card-img-overlay"
-              />
-              <Card.Body>
-                <Card.Title>{cards.blogTitle} <span><small className="text-muted customSmall" style={{fontSize:"12px"}}><span style={{color:"black"}}>@{cards.userName}</span></small></span></Card.Title>
-                <Card.Footer>
-                  <small className="text-muted customSmall"><span style={{color:"black"}}><i class="bi bi-person-square"></i> {cards.authorName}</span> <span>{cards.createdAt.slice(0,10)}</span></small>
-                </Card.Footer>
-                <br></br>
-                <Card.Text>
-                  {cards.aboutBlog.slice(0,140)}...
-                </Card.Text>
-                <LinkContainer  to={`/${cards._id}`}>
-                <Button variant="primary">Read <i className="bi bi-book"></i></Button>
-              </LinkContainer>
-                
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row> )}
-        </div>
-    )
+        <Row xs={1} sm={2} md={3} style={{ margin: '20px' }} className="g-4">
+          {blogs.map((cards) => (
+            <Col key={cards.createdAt}>
+              <Card className="customCard">
+                <Card.Img
+                  style={{ height: '40vh', width: '100%' }}
+                  variant="top"
+                  src={`${cards.file}`}
+                  bsPrefix="card-img-overlay"
+                />
+                <Card.Body>
+                  <Card.Title>
+                    {cards.blogTitle}{' '}
+                    <span>
+                      <small
+                        className="text-muted customSmall"
+                        style={{ fontSize: '12px' }}
+                      >
+                        <span style={{ color: 'black' }}>
+                          @{cards.userName}
+                        </span>
+                      </small>
+                    </span>
+                  </Card.Title>
+                  <Card.Footer>
+                    <small className="text-muted customSmall">
+                      <span style={{ color: 'black' }}>
+                        <i class="bi bi-person-square"></i> {cards.authorName}
+                      </span>{' '}
+                      <span>{cards.createdAt.slice(0, 10)}</span>
+                    </small>
+                  </Card.Footer>
+                  <br></br>
+                  <Card.Text>{cards.aboutBlog.slice(0, 140)}...</Card.Text>
+                  <LinkContainer to={`/${cards._id}`}>
+                    <Button variant="primary">
+                      Read <i className="bi bi-book"></i>
+                    </Button>
+                  </LinkContainer>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </div>
+  )
 }
 
 export default BlogList
